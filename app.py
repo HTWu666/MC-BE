9ed66ee8535f0493c585cade035a33478e4e619f
huitingwu <huitingwuuu@gmail.com>
2024-02-23 20:33:33 +0800

    refactor: set debug mode false

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def handle_exception(error) -> Tuple[Response, int]:
 
 # Run the Flask application
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
